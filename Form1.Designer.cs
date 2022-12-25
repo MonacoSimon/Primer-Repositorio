1fab26c5e7e22c3a7dcc4ec1e0141649739f1c46
MonacoSimon <simonmonaco86@gmailcom>
2022-12-25 20:59:11 -0300

    primera subida

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace ManejoExcepciones
+{
+    partial class FSuma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSuma = new System.Windows.Forms.Label();
+            this.txtboxPrimerNumero = new System.Windows.Forms.TextBox();
+            this.txtBoxSegundoNumero = new System.Windows.Forms.TextBox();
+            this.btResultado = new System.Windows.Forms.Button();
+            this.lblPrimerNumero = new System.Windows.Forms.Label();
+            this.lblSegundoNumero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSuma
+            // 
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Location = new System.Drawing.Point(26, 26);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(111, 13);
+            this.lblSuma.TabIndex = 0;
+            this.lblSuma.Text = "Ingrese dos numeros: ";
+            // 
+            // txtboxPrimerNumero
+            // 
+            this.txtboxPrimerNumero.Location = new System.Drawing.Point(183, 65);
+            this.txtboxPrimerNumero.Name = "txtboxPrimerNumero";
+            this.txtboxPrimerNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtboxPrimerNumero.TabIndex = 1;
+            // 
+            // txtBoxSegundoNumero
+            // 
+            this.txtBoxSegundoNumero.Location = new System.Drawing.Point(183, 139);
+            this.txtBoxSegundoNumero.Name = "txtBoxSegundoNumero";
+            this.txtBoxSegundoNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxSegundoNumero.TabIndex = 2;
+            // 
+            // btResultado
+            // 
+            this.btResultado.Location = new System.Drawing.Point(32, 222);
+            this.btResultado.Name = "btResultado";
+            this.btResultado.Size = new System.Drawing.Size(75, 23);
+            this.btResultado.TabIndex = 3;
+            this.btResultado.Text = "Resultado";
+            this.btResultado.UseVisualStyleBackColor = true;
+            this.btResultado.Click += new System.EventHandler(this.btResultado_Click);
+            // 
+            // lblPrimerNumero
+            // 
+            this.lblPrimerNumero.AutoSize = true;
+            this.lblPrimerNumero.Location = new System.Drawing.Point(29, 71);
+            this.lblPrimerNumero.Name = "lblPrimerNumero";
+            this.lblPrimerNumero.Size = new System.Drawing.Size(82, 13);
+            this.lblPrimerNumero.TabIndex = 4;
+            this.lblPrimerNumero.Text = "Primer Numero: ";
+            // 
+            // lblSegundoNumero
+            // 
+            this.lblSegundoNumero.AutoSize = true;
+            this.lblSegundoNumero.Location = new System.Drawing.Point(32, 145);
+            this.lblSegundoNumero.Name = "lblSegundoNumero";
+            this.lblSegundoNumero.Size = new System.Drawing.Size(96, 13);
+            this.lblSegundoNumero.TabIndex = 5;
+            this.lblSegundoNumero.Text = "Segundo Numero: ";
+            // 
+            // FSuma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 341);
+            this.Controls.Add(this.lblSegundoNumero);
+            this.Controls.Add(this.lblPrimerNumero);
+            this.Controls.Add(this.btResultado);
+            this.Controls.Add(this.txtBoxSegundoNumero);
+            this.Controls.Add(this.txtboxPrimerNumero);
+            this.Controls.Add(this.lblSuma);
+            this.Name = "FSuma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicacion Suma";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSuma;
+        private System.Windows.Forms.TextBox txtboxPrimerNumero;
+        private System.Windows.Forms.TextBox txtBoxSegundoNumero;
+        private System.Windows.Forms.Button btResultado;
+        private System.Windows.Forms.Label lblPrimerNumero;
+        private System.Windows.Forms.Label lblSegundoNumero;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManejoExcepciones
+{
+    public partial class FSuma : Form
+    {
+        public FSuma()
+        {
+            InitializeComponent();
+        }
+
+        private void btResultado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a, b, r;
+
+                a = int.Parse(txtboxPrimerNumero.Text);
+
+                b = int.Parse(txtBoxSegundoNumero.Text);
+
+                r = a + b;
+
+                MessageBox.Show("el resultado es: " + r);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("ingreso caracteres incorrectos. vuelva a intentar");
+            }
+        }
+    }
+}
